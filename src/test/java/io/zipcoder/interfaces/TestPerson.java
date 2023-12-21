06606b1642571a56afe4df9b5dd587747cff174e
Loc Pham <148402164+locpham126@users.noreply.github.com>
2023-12-20 21:27:31 -0500

    created testperson class

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,22 @@
 package io.zipcoder.interfaces;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestPerson {
+    @Test
+    public void testConstructor(){
+        Person person = new Person(1, "Bob");
+        long expected = 1L;
+        String expectedName = "Bob";
+        Assert.assertEquals(expected, person.getId());
+        Assert.assertEquals(expectedName, person.getName());
+    }
 
+    @Test
+    public void testSetName(){
+        Person person = new Person(1, "Bob");
+        String expected = "Blob";
+        person.setName("Blob");
+        Assert.assertEquals(expected, person.getName());
+    }
 }
